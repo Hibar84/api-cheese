08a273fcfa7d67acba6727eff2d212691f3915f9
Hibar84 <lucas.guilhen67@gmail.com>
2022-07-20 18:23:42 +0200

    Ajout de l'autorisation CORS

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
+const cors = require('cors');
 const functions = require("firebase-functions");
 const app = require("express")();
     
 const CHEESE = require("./api/routes/cheese");
 
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Utilisation de l'API concernant le fromage
 app.use('/cheese',CHEESE);
 
 exports.api = functions.region("europe-west3").https.onRequest(app);
